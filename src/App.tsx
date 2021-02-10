9069c58dab81f5dcacd3060d6f64b479975ced52
Haixiang <haixiang6123@gmail.com>
2021-02-10 11:23:29 +0800

    feat: show and hide front

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Cover from 'lib/Cover';
 import sprite from 'assets/sprite.jpg';
+import content from 'assets/content.webp';
 
 function App() {
   return (
     <div className="App">
       <Cover width={160} height={90} rows={10} cols={10} previewSprite={sprite}>
-        Hello
+        <img style={{ height: 90, width: 160 }} src={content} alt=""/>
       </Cover>
     </div>
   );
--- src/lib/Cover/index.tsx
+++ src/lib/Cover/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import {useCallback, useRef} from 'react'
+import {MouseEventHandler, useCallback, useRef} from 'react'
 import './styles.scss'
 
 interface Props {
@@ -13,12 +13,13 @@ interface Props {
 const Cover: React.FC<Props> = (props) => {
   const {children, width, height, rows, cols, previewSprite} = props
 
-  const $cover = useRef<HTMLDivElement>(null);
-  const $progress = useRef<HTMLDivElement>(null);
-  const $preview = useRef<HTMLDivElement>(null);
+  const $cover = useRef<HTMLDivElement>(null)
+  const $front = useRef<HTMLDivElement>(null);
+  const $progress = useRef<HTMLDivElement>(null)
+  const $preview = useRef<HTMLDivElement>(null)
 
   // Calculate how many preview pictures in the sprite pic
-  const totalPicNum = rows * cols;
+  const totalPicNum = rows * cols
 
   const baseStyle = {
     width,
@@ -30,33 +31,50 @@ const Cover: React.FC<Props> = (props) => {
     backgroundImage: `url("${previewSprite}")`,
   }
 
-  const onMouseMove = useCallback((e) => {
-    if (!$cover.current || !$progress.current || !$preview.current) return;
+  const onMouseEnter: MouseEventHandler = useCallback(() => {
+    if (!$front.current) return
+
+    // Show front
+    $front.current.style.opacity = '1'
+  }, [])
+
+  const onMouseLeave: MouseEventHandler = useCallback(() => {
+    if (!$front.current) return
+
+    $front.current.style.opacity = '0'
+  }, [])
+
+  const onMouseMove: MouseEventHandler = useCallback((e) => {
+    if (!$cover.current || !$progress.current || !$preview.current) return
 
     // Update progress
     const rect = $cover.current.getBoundingClientRect()
-    const offsetX = Math.abs(e.pageX - rect.left); // The x offset of mouse with 'cover' div element
-    const percentage = offsetX / rect.width;
-    const progress = percentage * 100;
-    $progress.current.style.width = `${progress}%`;
+    const offsetX = Math.abs(e.pageX - rect.left) // The x offset of mouse with 'cover' div element
+    const percentage = offsetX / rect.width
+    const progress = percentage * 100
+    $progress.current.style.width = `${progress}%`
 
     // Update preview pic
-    const curtPic = Math.round(totalPicNum * percentage); // Looking for the curt preview pic
+    const curtPic = Math.round(totalPicNum * percentage) // Looking for the curt preview pic
     const curtPicPos = { // Position of curt preview pic in the sprite pic
       x: (curtPic % cols) * width,
       y: (Math.floor(curtPic / cols)) * height
-    };
-    $preview.current.style.backgroundPosition = `-${curtPicPos.x}px -${curtPicPos.y}px`;
+    }
+    $preview.current.style.backgroundPosition = `-${curtPicPos.x}px -${curtPicPos.y}px`
   }, [])
 
   return (
-    <div ref={$cover} style={baseStyle} className="cover" onMouseMove={onMouseMove}>
+    <div ref={$cover} style={baseStyle}
+         className="cover"
+         onMouseEnter={onMouseEnter}
+         onMouseLeave={onMouseLeave}
+         onMouseMove={onMouseMove}>
       {/* The elements that are in the front */}
-      <div className="front">
+      <div ref={$front} className="front">
         {/* Header progress bar*/}
         <div className="header">
           <div className="track">
-            <div ref={$progress} className="progress" />
+            <div ref={$progress} className="progress"/>
           </div>
         </div>
 
